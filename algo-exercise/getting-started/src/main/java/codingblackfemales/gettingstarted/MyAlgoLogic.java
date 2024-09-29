e407bd8796af638b9475b449a61b279bb82124b7
@sgnahore <stephaniergnahore@gmail.com>
2024-09-29 17:37:04 +0100

    repeat order creation issue resolved

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -48,30 +48,37 @@ public class MyAlgoLogic implements AlgoLogic {
         final BidLevel highestBid = state.getBidAt(0);
         final AskLevel lowestAsk = state.getAskAt(0);
         final long spread = lowestAsk.price - highestBid.price;
-        final long spreadThreshold = 3000;
+        final long spreadThreshold = 5;
 
         //timestamps variables
         Timestamp ts = new Timestamp(System.currentTimeMillis());
         final var activeOrders = state.getActiveChildOrders();
         logger.info("[MYALGO] Current time stamps " + timestamps);
 
+        final var option = activeOrders.stream().findFirst();
 
-            // If we have fewer than 3 child orders, we want to add new ones
-            if (totalOrderCount < 10) {
-                logger.info("[MYALGO] Have:" + state.getChildOrders().size() + " children, want 3");
+        //exit condition
+        if (spread > spreadThreshold) {
+            logger.info("[MYALGO] Exiting market, spread is too wide to trade! Current spread: " + spread);
+            return NoAction.NoAction;
+        }
 
-                //exit condition
-                if (spread > spreadThreshold) {
-                    logger.info("[MYALGO] Exiting market, spread is too wide to trade! Current spread: " + spread);
-                    return NoAction.NoAction;
-                } else{
+            // If we have fewer than 4 child orders, we want to add new ones
+            if (totalOrderCount < 10 && option.isEmpty()) {
+                logger.info("[MYALGO] Have:" + state.getChildOrders().size() + " children, want 3" );
+
+
                     //check if any askprice is less than vwap threshold
-
                     for (int i = 0; i < state.getAskLevels(); i++) {
                     long askPrice = state.getAskAt(i).price;
                     long askQuantity = state.getAskAt(i).quantity;
                     final AskLevel currentAskLevel = state.getAskAt(i);
 
+                    long bidQuantity = state.getBidAt(i).quantity;
+                    long bidPrice = state.getBidAt(i).price;
+                    final BidLevel currentBidLevel = state.getBidAt(i);
+
+                //create a buy order if ask price is good
                     if (vwap - vwapThreshold >= askPrice) {
 
                         logger.info("[MYALGO] Volume-Weighted Av Price is " + vwap);
@@ -80,24 +87,37 @@ public class MyAlgoLogic implements AlgoLogic {
                         timestamps.add(ts);
                         return new CreateChildOrder(Side.BUY, askQuantity, askPrice) ;
 
-                        //if order older than one minute, cancel
                     } else {
                         logger.info("[MYALGO] Cannot trade, price too high ");
                     }
 
-                    //if order has been present ON PASSIVE SIDE for longer than 60 milliseconds, cancel the order
-                    if (!timestamps.isEmpty()) {
-                        for (int j = 0; j < timestamps.size(); j++ ){
 
-                            long timeDifference = ts.getTime() - timestamps.get(j).getTime();
-                            if (timeDifference > 60) {
-                                logger.info("[MYALGO] Cancelling order at level " + j + " order is older than 1 minute: " + timestamps.get(j));
-                                return new CancelChildOrder(activeOrders.get(j)) ;
+                    }
+
+                //if bid price is good, create sell order
+                } for (int i = 0; i < state.getBidLevels(); i++) {
+                    long bidPrice = state.getBidAt(i).price;
+                    long bidQuantity = state.getBidAt(i).quantity;
+
+                    if (bidPrice >= vwap + vwapThreshold) {
+                        logger.info("[MYALGO] VWAP Sell Condition Met. Creating child sell order.");
+                        timestamps.add(ts);
+                        return new CreateChildOrder(Side.SELL, bidQuantity, bidPrice);
+                    }
+                }
+
+                //if order has been present ON PASSIVE SIDE for longer than 6 milliseconds, cancel the order
+                 if (!timestamps.isEmpty()) {
+                    for (int j = 0; j < timestamps.size(); j++ ){
+
+                        long timeDifference = ts.getTime() - timestamps.get(j).getTime();
+                        if (timeDifference > 600) {
+                            logger.info("[MYALGO] Cancelling order at level " + j + " order is older than 1 minute: " + timestamps.get(j));
+                            return new CancelChildOrder(activeOrders.get(j)) ;
 
                         } else {
-                                logger.info("[MYALGO] No order to cancel ");
-                            }
-                    }}
+                            logger.info("[MYALGO] No order to cancel ");
+
                     }
                 }
             } else {
